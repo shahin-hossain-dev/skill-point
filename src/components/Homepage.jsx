56e2b20e80810134997d48046c383d80908313fc
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-10-06 22:40:16 +0600

    project initialized

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <Button className="bg-primary-color text-white hover:bg-secondary-color">
+        Click Me
+      </Button>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="mx-2 md:mx-8 py-3">
+      <h2 className="text-primary-color text-3xl font-bold ">
+        <span className="text-secondary-color">Skill</span> Point
+      </h2>
+    </div>
+  );
+};
+
+export default Navbar;
